57dfe9de4c6aef6669c4f0f57bb010c3e19ac44b
vishnu <xyz@gmail.com>
2023-12-21 23:25:44 +0530

    wages

--- com/dayfourassign/EmployeeWageComputation.java
+++ com/dayfourassign/EmployeeWageComputation.java
@@ -1,9 +1,45 @@
 package com.dayfourassign;
-
-public class AddNum {
+import java.sql.SQLOutput;
+import java.util.Random;
+public class EmployeeWageComputation {
     public static void main(String[] args) {
-        int a =10;
-        int b =20;
-        System.out.println(a+b);
-    }
+
+
+        int day = 0;
+        Random random = new Random();
+       int  fulltime = 1;
+
+        int randomNumber = random.nextInt(100) + 1;
+        double empCheck = randomNumber % 2;
+        //double empCheck = Math.floor(Math.random() + 10) % 2;
+        for(int i = 0; i<=19; i++) {
+            if (fulltime == empCheck) {
+
+                System.out.println("employee is present");
+                day++;
+
+
+            }
+             else {
+                System.out.println("employee is absent");
+                break;
+
+            }
+        }
+
+            switch (day) {
+                case 20:
+                    System.out.println("one month salary " + day * 160);
+                    break;
+                case 1:
+                    System.out.println("one day salary " + day * 160);
+                    break;
+
+
+                default:
+                    System.out.println("employeee need to work more");
+            }
+        }
+
+
 }
